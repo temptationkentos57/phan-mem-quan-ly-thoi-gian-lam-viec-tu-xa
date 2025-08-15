dbeaeb99eda574f1026338c1909eefa2064f9b51
temptationkentos57 <kreetlannoo@hotmail.com>
2025-08-15 13:07:23 +0200

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let tasks = [];
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.post('/tasks', (req, res) => {
+  const task = req.body;
+  tasks.push(task);
+  res.status(201).json(task);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
